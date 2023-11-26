0fcdab682378fcd45acbfc4affc731e206b0ee46
Yasin Hussein <bulaale116@gmail.com>
2023-11-26 10:02:14 -0700

    using export module

---
+++ client.js
@@ -0,0 +1,19 @@
+/* eslint-disable linebreak-style */
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function(data) {
+  const conn = net.createConnection({
+    host: 'localhost',// IP address here,
+    port: 3000, // PORT number here,
+  });
+  conn.on("connect", (data) => {
+    console.log(data);
+    // code that does something when the connection is first established
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-undef */
 /* eslint-disable semi */
-const net = require("net");
+const cnn = require('./client.js');
 
-// establishes a connection with the game server
-const connect = function(data) {
-  const conn = net.createConnection({
-    host: 'localhost',// IP address here,
-    port: 3000, // PORT number here,
-  });
-  conn.on("connect", (data) => {
-    console.log(data);
-    // code that does something when the connection is first established
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
+cnn();
 console.log("Connecting ...");
-connect();
+// connect();
